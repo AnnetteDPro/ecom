606a3a446b72c61314eb46fc6521bf6264cfa536
AnnetteDPro <112832201+AnnetteDPro@users.noreply.github.com>
2022-09-07 14:15:22 +0200

    Update DATASET.py

--- streamlit_app/tabs/DATASET.py
+++ streamlit_app/tabs/DATASET.py
@@ -1,8 +1,10 @@
+$ python3
+
 import streamlit as st
 import pandas as pd
 import numpy as np
 
-python -m pip install seaborn
+pip install seaborn
 from PIL import Image
 
 import plotly.graph_objects as go
