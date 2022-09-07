69d01a129fdd19321f2a323fc8ec91141889893f
AnnetteDPro <112832201+AnnetteDPro@users.noreply.github.com>
2022-09-07 15:46:49 +0200

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,3 +1,10 @@
+echo "# streamlit-to-heroku-tutorial" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/your_username/your_repo_name.git
+git push -u origin master
 from collections import OrderedDict
 
 import streamlit as st
