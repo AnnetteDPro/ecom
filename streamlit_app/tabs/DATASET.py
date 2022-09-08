a37b7e6855e3c4c7c3ada7c73ddc054124dc19eb
AnnetteDPro <112832201+AnnetteDPro@users.noreply.github.com>
2022-09-08 13:31:15 +0200

    Update DATASET.py

--- streamlit_app/tabs/DATASET.py
+++ streamlit_app/tabs/DATASET.py
@@ -8,8 +8,13 @@ import matplotlib.pyplot as plt
 
 title = "Projet Estimation de ventes e-commerce"
 sidebar_name = "DATASET"
+from google.colab import drive
+drive.mount('/content/drive')
 
-df = pd.read_csv("https://wetransfer.com/downloads/659bcf0aa3862ec2997f437463abcadc20220907142858/dfed41")
+path = '/content/drive/MyDrive/Data analyste/Projet/Pakistan Largest Ecommerce Dataset.csv'
+
+df = pd.read_csv(path)
+
 
 def run():
     @st.cache
