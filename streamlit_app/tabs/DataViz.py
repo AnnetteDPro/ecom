b8b40ce793f4a41859c500befcfc72b6cf71425e
AnnetteDPro <112832201+AnnetteDPro@users.noreply.github.com>
2022-09-12 09:28:08 +0200

    Update DataViz.py

--- streamlit_app/tabs/DataViz.py
+++ streamlit_app/tabs/DataViz.py
@@ -25,7 +25,10 @@ Pour le moment, cela reste des chiffres ou des informations brutes. La deuxième
         """)
     @st.cache
     def fin_data ():
-        df = pd.read_csv("https://www.dropbox.com/s/4zy7dmd6dc9ccjo/Pakistan%20Largest%20Ecommerce%20Dataset.csv?dl=0")
+        
+    import zipfile
+    with zipfile.ZipFile("Pakistan Largest Ecommerce Dataset.csv.zip","python") as zip_ref:
+         zip_ref.extractall("df")
 
 
       #suppression des lignes et des colonnes vides
