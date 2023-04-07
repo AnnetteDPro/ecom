a29ed4881ae948143e8f1f104b180f931e912bfa
AnnetteDPro <dubusanna@gmail.com>
2023-04-07 20:02:56 +0200

    Add app.py

---
+++ config.py
@@ -0,0 +1,22 @@
+"""
+
+Config file for Streamlit App
+
+"""
+
+from member import Member
+
+
+TITLE = "E-COM"
+
+TEAM_MEMBERS = [
+    Member(
+        name="Annette Dubus",
+        linkedin_url="https://www.linkedin.com/in/annadubus",
+        github_url="https://github.com/charlessutton",
+    ),
+    Member("TO Thi Phuong Thao"),
+    Member("Camille LARA"),
+]
+
+PROMOTION = "Cohorte Data-Analystes février 2022 de DataScientest"
