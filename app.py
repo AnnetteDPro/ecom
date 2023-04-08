09fb7f4031024397ac535da7469633e530ab32e4
AnnetteDPro <112832201+AnnetteDPro@users.noreply.github.com>
2023-04-09 00:20:05 +0200

    test

--- app.py
+++ app.py
@@ -17,37 +17,6 @@ with open("style.css", "r") as f:
 st.markdown(f"<style>{style}</style>", unsafe_allow_html=True)
 
 
-# TODO: add new and/or renamed tab in this ordered dict by
-# passing the name in the sidebar as key and the imported tab
-# as value as follow :
-TABS = OrderedDict(
-    [
-        (DATASET.sidebar_name, DATASET),
-        (DataViz.sidebar_name, DataViz),
-        (RFM.sidebar_name, RFM),
-        (Kmeans.sidebar_name, Kmeans),
-        (Prediction.sidebar_name, Prediction),
-        
-    ]
-)
-
-
-def run():
-    st.sidebar.image(
-        "https://studio.datascientest.com/static/images/logo_header.svg",
-        width=300,
-    )
-    tab_name = st.sidebar.radio("", list(TABS.keys()), 0)
-    st.sidebar.markdown("---")
-    st.sidebar.markdown(f"## {config.PROMOTION}")
-
-    st.sidebar.markdown("### Team members:")
-    for member in config.TEAM_MEMBERS:
-        st.sidebar.markdown(member.sidebar_markdown(), unsafe_allow_html=True)
-
-    tab = TABS[tab_name]
-
-    tab.run()
 
  
 if __name__ == "__main__":
