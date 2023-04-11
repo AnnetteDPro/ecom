d242a414ebe364ec4b5e4fe8838074ec9f7b7b03
AnnetteDPro <112832201+AnnetteDPro@users.noreply.github.com>
2023-04-12 00:06:57 +0200

    modif1

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import streamlit as st
 # TODO : change TITLE, TEAM_MEMBERS and PROMOTION values in config.py.
 import config
 
+from tabs import DATASET, DataViz, RFM, Kmeans, Prediction
+
 st.set_page_config(
     page_title=config.TITLE,
     page_icon="https://datascientest.com/wp-content/uploads/2020/03/cropped-favicon-datascientest-1-32x32.png",
@@ -19,7 +21,16 @@ st.markdown(f"<style>{style}</style>", unsafe_allow_html=True)
 # TODO: add new and/or renamed tab in this ordered dict by
 # passing the name in the sidebar as key and the imported tab
 # as value as follow :
-
+TABS = OrderedDict(
+    [
+        (DATASET.sidebar_name, DATASET),
+        (DataViz.sidebar_name, DataViz),
+        (RFM.sidebar_name, RFM),
+        (Kmeans.sidebar_name, Kmeans),
+        (Prediction.sidebar_name, Prediction),
+        
+    ]
+)
 
 
 def run():
@@ -27,7 +38,7 @@ def run():
         "https://studio.datascientest.com/static/images/logo_header.svg",
         width=300,
     )
-    tab_name = st.sidebar.radio("", list(tabs.keys()), 0)
+    tab_name = st.sidebar.radio("", list(TABS.keys()), 0)
     st.sidebar.markdown("---")
     st.sidebar.markdown(f"## {config.PROMOTION}")
 
@@ -35,7 +46,7 @@ def run():
     for member in config.TEAM_MEMBERS:
         st.sidebar.markdown(member.sidebar_markdown(), unsafe_allow_html=True)
 
-    tab = tabs[tab_name]
+    tab = TABS[tab_name]
 
     tab.run()
 
