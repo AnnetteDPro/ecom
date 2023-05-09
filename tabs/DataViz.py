4b8cb5e207d477e9f013f5239f72084f168ae5eb
AnnetteDPro <112832201+AnnetteDPro@users.noreply.github.com>
2023-05-09 15:03:57 +0200

    Update DataViz.py

--- tabs/DataViz.py
+++ tabs/DataViz.py
@@ -34,7 +34,7 @@ Pour le moment, cela reste des chiffres ou des informations brutes. La deuxième
     df_2 = pd.read_csv("https://www.dropbox.com/s/vtlr8jubvopw4v9/ecom_df_2.csv?dl=1")
    
      
-    df_2["created_at"] = pd.to_datetime(df_2["created_at"])
+    df_2["created_at"] = pd.to_datetime(df_2["created_at"], format='ISO8601')
         
       
     st.markdown("""
